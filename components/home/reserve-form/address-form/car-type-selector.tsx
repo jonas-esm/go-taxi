788d2a91d56e0da916741ac13bd4bd89b8f5e676
@anas58 <anas@finflx.com>
2024-09-11 16:01:20 +0300

    add car type image to selector

--- components/home/reserve-form/address-form/car-type-selector.tsx
+++ components/home/reserve-form/address-form/car-type-selector.tsx
@@ -1,166 +1,100 @@
-import React from "react";
+import React from 'react'
 
-import { Box, Stack, Typography, useTheme, type Palette } from "@mui/material";
+import { Box, Stack, Typography, useTheme, type Palette } from '@mui/material'
 
-import { Controller, useFormContext } from "react-hook-form";
+import { Controller, useFormContext } from 'react-hook-form'
 
-import { Icon } from "@iconify-icon/react/dist/iconify.mjs";
+import { Icon } from '@iconify-icon/react/dist/iconify.mjs'
 
-import { carTypeOptions } from "@/utils/car-selector-data";
-import { formatTime, formatDistance } from "@/utils/fetch-address.utils";
+import { carTypeOptions } from '@/utils/car-selector-data'
+import { formatTime, formatDistance } from '@/utils/fetch-address.utils'
 
-import type { ReservationFormData } from "..";
+import type { ReservationFormData } from '..'
 
-const getTextColor = (
-    item: { label: string },
-    value: string,
-    palette: Palette
-) => ({
-    color: item.label !== value ? palette.text.primary : "#fff",
-});
+const getTextColor = (item: { label: string }, value: string, palette: Palette) => ({
+  color: item.label !== value ? palette.text.primary : '#fff'
+})
 
-function CarTypeSelector({
-    isFetchingTripDetails,
-}: {
-    isFetchingTripDetails?: boolean;
-}) {
-    const { palette } = useTheme();
-    const { control, getValues } = useFormContext<ReservationFormData>();
+function CarTypeSelector({ isFetchingTripDetails }: { isFetchingTripDetails?: boolean }) {
+  const { palette } = useTheme()
+  const { control, getValues } = useFormContext<ReservationFormData>()
 
-    return (
-        <Controller
-            control={control}
-            name="carType"
-            render={({ field: { value, onChange } }) => (
-                <Stack>
-                    {carTypeOptions.map((item, ind) => (
-                        <Box
-                            key={ind}
-                            sx={{
-                                px: 3,
-                                py: 4,
-                                borderRadius: 1.5,
-                                background:
-                                    item.label === value
-                                        ? palette.info.main
-                                        : "#F7F8FA",
+  return (
+    <Controller
+      control={control}
+      name='carType'
+      render={({ field: { value, onChange } }) => (
+        <Stack>
+          {carTypeOptions.map((item, ind) => (
+            <Box
+              key={ind}
+              sx={{
+                px: 3,
+                py: 4,
+                borderRadius: 1.5,
+                background: item.label === value ? palette.info.main : '#F7F8FA',
 
-                                mb: 2,
-                                border: `1px solid ${palette.background.paper}`,
+                mb: 2,
+                border: `1px solid ${palette.background.paper}`,
 
-                                cursor: "pointer",
-                                " &:hover":
-                                    item.label !== value
-                                        ? {
-                                              background: "#f8f8f8",
-                                              border: `1px solid ${palette.primary.main}`,
-                                          }
-                                        : undefined,
-                            }}
-                            onClick={() => {
-                                console.log(item);
-                                onChange(item.label);
-                            }}
-                        >
-                            <Stack
-                                direction={"row"}
-                                justifyContent={"space-between"}
-                            >
-                                <Box>
-                                    <Stack direction={"row"}>
-                                        <Typography
-                                            color={
-                                                getTextColor(
-                                                    item,
-                                                    value,
-                                                    palette
-                                                ).color
-                                            }
-                                            variant="h4"
-                                            mr={2}
-                                        >
-                                            {item.label}
-                                        </Typography>
+                cursor: 'pointer',
+                ' &:hover':
+                  item.label !== value
+                    ? {
+                        background: '#f8f8f8',
+                        border: `1px solid ${palette.primary.main}`
+                      }
+                    : undefined
+              }}
+              onClick={() => {
+                console.log(item)
+                onChange(item.label)
+              }}
+            >
+              <Stack direction={'row'} justifyContent={'space-between'}>
+                {item.carImage}
+                <Box mr={'auto'} ml={2}>
+                  <Stack direction={'row'}>
+                    <Typography color={getTextColor(item, value, palette).color} variant='h4' mr={2}>
+                      {item.label}
+                    </Typography>
 
-                                        {/* {item.peopleIcon} */}
-                                        <item.peopleIcon
-                                            color={
-                                                getTextColor(
-                                                    item,
-                                                    value,
-                                                    palette
-                                                ).color
-                                            }
-                                        />
-                                        <Typography
-                                            color={
-                                                getTextColor(
-                                                    item,
-                                                    value,
-                                                    palette
-                                                ).color
-                                            }
-                                            display={"inline"}
-                                            ml={1}
-                                            sx={getTextColor(
-                                                item,
-                                                value,
-                                                palette
-                                            )}
-                                        >
-                                            {item.capacity}
-                                        </Typography>
-                                    </Stack>
-                                    <Typography
-                                        color={
-                                            getTextColor(item, value, palette)
-                                                .color
-                                        }
-                                    >
-                                        {item.discription}
-                                    </Typography>
-                                </Box>
+                    {/* {item.peopleIcon} */}
+                    <item.peopleIcon color={getTextColor(item, value, palette).color} />
+                    <Typography
+                      color={getTextColor(item, value, palette).color}
+                      display={'inline'}
+                      ml={1}
+                      sx={getTextColor(item, value, palette)}
+                    >
+                      {item.capacity}
+                    </Typography>
+                  </Stack>
+                  <Typography color={getTextColor(item, value, palette).color}>{item.discription}</Typography>
+                </Box>
 
-                                {isFetchingTripDetails ? (
-                                    <Icon
-                                        icon={"eos-icons:three-dots-loading"}
-                                        color={palette.primary.main}
-                                        style={{
-                                            color: palette.primary.main,
-                                            fontSize: 40,
-                                        }}
-                                    />
-                                ) : getValues().tripDetails?.routes[0]
-                                      ?.distance ? (
-                                    <Box
-                                        ml={"auto"}
-                                        color={getTextColor(
-                                            item,
-                                            value,
-                                            palette
-                                        )}
-                                    >
-                                        <div className="text-black ">
-                                            {formatTime(
-                                                getValues().tripDetails
-                                                    ?.routes[0].duration || 0
-                                            )}
-                                        </div>
-                                        <div className="text-black">
-                                            {formatDistance(
-                                                getValues().tripDetails
-                                                    ?.routes[0].distance || 0
-                                            )}
-                                        </div>
-                                    </Box>
-                                ) : null}
-                            </Stack>
-                        </Box>
-                    ))}
-                </Stack>
-            )}
-        />
-    );
+                {isFetchingTripDetails ? (
+                  <Icon
+                    icon={'eos-icons:three-dots-loading'}
+                    color={palette.primary.main}
+                    style={{
+                      color: palette.primary.main,
+                      fontSize: 40
+                    }}
+                  />
+                ) : getValues().tripDetails?.routes[0]?.distance ? (
+                  <Box ml={'auto'} color={getTextColor(item, value, palette)}>
+                    <div className='text-black '>{formatTime(getValues().tripDetails?.routes[0].duration || 0)}</div>
+                    <div className='text-black'>{formatDistance(getValues().tripDetails?.routes[0].distance || 0)}</div>
+                  </Box>
+                ) : null}
+              </Stack>
+            </Box>
+          ))}
+        </Stack>
+      )}
+    />
+  )
 }
 
-export default CarTypeSelector;
+export default CarTypeSelector
--- utils/car-selector-data.tsx
+++ utils/car-selector-data.tsx
@@ -1,35 +1,39 @@
-import { Icon } from "@iconify-icon/react/dist/iconify.mjs";
+import Image from 'next/image'
+
+import { Icon } from '@iconify-icon/react/dist/iconify.mjs'
+
+import { Box } from '@mui/material'
+
+import FamilyCarImage from '@/assets/family_car.png'
+
+import NormalCarImage from '@/assets/normal_car.png'
 
 export const carTypeOptions = [
-    {
-        label: "LUXURY",
-        discription: "Comfort, luxurious design",
-        carImage: "",
-        peopleIcon: ({ color }: { color: string }) => (
-            <Icon icon="material-symbols:man" width={20} style={{ color }} />
-        ),
-        capacity: "4",
-    },
-    {
-        label: "FAMILY",
-        discription: "Big, sturdy, family friendly",
-        carImage: "",
-        peopleIcon: ({ color }: { color: string }) => (
-            <Icon
-                icon="material-symbols:family-restroom"
-                width={20}
-                style={{ color }}
-            />
-        ),
-        capacity: "8",
-    },
-    {
-        label: "ECONOMY",
-        discription: "Practicality, comfort, everyday use",
-        carImage: "",
-        peopleIcon: ({ color }: { color: string }) => (
-            <Icon icon="material-symbols:man" width={20} style={{ color }} />
-        ),
-        capacity: "4",
-    },
-];
+  {
+    label: 'LUXURY',
+    discription: 'Comfort, luxurious design',
+    carImage: <Image src={NormalCarImage} alt='' />,
+    peopleIcon: ({ color }: { color: string }) => <Icon icon='material-symbols:man' width={20} style={{ color }} />,
+    capacity: '4'
+  },
+  {
+    label: 'FAMILY',
+    discription: 'Big, sturdy, family friendly',
+    carImage: (
+      <Box ml={-4}>
+        <Image src={FamilyCarImage} alt='' />
+      </Box>
+    ),
+    peopleIcon: ({ color }: { color: string }) => (
+      <Icon icon='material-symbols:family-restroom' width={20} style={{ color }} />
+    ),
+    capacity: '8'
+  },
+  {
+    label: 'ECONOMY',
+    discription: 'Practicality, comfort, everyday use',
+    carImage: <Image src={NormalCarImage} alt='' />,
+    peopleIcon: ({ color }: { color: string }) => <Icon icon='material-symbols:man' width={20} style={{ color }} />,
+    capacity: '4'
+  }
+]
